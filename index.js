3765bb1552404a59fe1ca54ca46c5b4fddacf36e
Abheet seth <114290748+AbheetHacker4278@users.noreply.github.com>
2024-01-26 01:16:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+const Api_key = "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";
+let clkbtn = document.querySelector('.submit-icon');
+let inputelem = document.querySelector('.inpt');
+let imagesection = document.querySelector('.images-section');
+let loader = document.querySelector('.loader');
+let loader2 = document.querySelector('.loader2');
+let esra = document.querySelector('.error');
+
+document.addEventListener('DOMContentLoaded', () => {
+    clkbtn.addEventListener('click', async () => {
+        imagesection.innerHTML = '';
+        loader.style.display = 'block';
+        loader2.style.display = 'none';
+        esra.style.display = 'none';
+        const options = {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${Api_key}`,
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                "prompt": inputelem.value,
+                "n": 1,
+                "size": "1024x1024"
+            })
+        }
+
+        try {
+            let getdata = await fetch('https://api.openai.com/v1/images/generations', options);
+            let struct = await getdata.json();
+
+            setTimeout(()=>{
+                struct?.data.forEach(imageobj =>{
+                    const imagecontainer = document.createElement('div');
+                    imagecontainer.classList.add('imagecontainer');
+                    const imageElement = document.createElement('img');
+                    imageElement.setAttribute('src' , imageobj.url);
+                    imagecontainer.append(imageElement);
+                    imagesection.append(imagecontainer);
+                })
+                loader.style.display = 'none';
+            } , 3000)
+            
+            // console.log(struct);
+            if(struct?.error?.code == "content_policy_violation"){
+                esra.style.display = 'none';
+                loader.style.display = 'none';
+                loader2.style.display = 'block';
+            }
+            else if(struct?.created){
+                loader.style.display = 'none';
+                loader2.style.display = 'none';
+                esra.style.display = 'none';
+            }
+            else{
+                esra.style.display = 'block';
+                loader.style.display = 'none';
+            }
+        } 
+        catch (error) {
+            console.log("error caught");
+        }
+    });
+});
+
+document.addEventListener("contextmenu", (e)=>{
+    e.preventDefault();
+},false);
